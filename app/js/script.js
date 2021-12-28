13b343d0f8e2721be3eb7c5b2a214a7db812fc2b
Paulo Andrade <paulobacelara@gmail.com>
2021-12-28 14:45:54 -0300

    Added open/close logic to burger button

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,6 @@
-console.log("Hello World!");
+const btnHamburger = document.querySelector("#btnHamburger");
+
+btnHamburger.addEventListener("click", () => {
+  console.log("open hamburger");
+  btnHamburger.classList.toggle("open");
+});
