174c6b16c083b2e46625bc0f96ee92fbf67f6909
Paulo Andrade <paulobacelara@gmail.com>
2021-12-29 06:59:41 -0300

    Created and styled menu for mobile

--- app/js/script.js
+++ app/js/script.js
@@ -1,15 +1,24 @@
 const btnHamburger = document.querySelector("#btnHamburger");
+const body = document.querySelector("body");
 const header = document.querySelector(".header");
-const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
 
 btnHamburger.addEventListener("click", () => {
   if (header.classList.contains("open")) {
     header.classList.remove("open");
-    overlay.classList.remove("fade-in");
-    overlay.classList.add("fade-out");
+    body.classList.remove("noscroll");
+
+    fadeElems.forEach((elem) => {
+      elem.classList.remove("fade-in");
+      elem.classList.add("fade-out");
+    });
   } else {
     header.classList.add("open");
-    overlay.classList.remove("fade-out");
-    overlay.classList.add("fade-in");
+    body.classList.add("noscroll");
+
+    fadeElems.forEach((elem) => {
+      elem.classList.remove("fade-out");
+      elem.classList.add("fade-in");
+    });
   }
 });
